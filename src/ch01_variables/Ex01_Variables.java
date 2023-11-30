1ba1a9607bcce61ecc3fcb07e404486c761504d1
Taein142 <xodlsdldy@naver.com>
2023-11-30 14:24:50 +0900

    변수 선언 및 확인

---
+++ src/ch01_variables/Ex01_Variables.java
@@ -0,0 +1,24 @@
+package ch01_variables;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 본인의 이름(name), 나이(age), 키(height), 주소(address),
+		// 전화번호(mobile)를 입력할 수 있는 변수를 각각 선언해봅시다.
+		String name = "김태인";
+		int age = 26;
+		double height = 173.4;
+		String address = "인천광역시 미추홀구 재넘이길 29번길 75-12(주택)";
+		String mobile = "010-5809-5132";
+		
+		//	변수값 출력
+		System.out.println(address);
+		System.out.println(name);
+		System.out.println(mobile);
+//		System.out.println(height1);
+		System.out.println("저의 이름은 " + name + "입니다.");
+		
+	}
+
+}
