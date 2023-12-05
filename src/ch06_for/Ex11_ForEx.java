aa4a4d4837142bfc891422055ac25efc85c4bf6a
Taein142 <xodlsdldy@naver.com>
2023-12-05 15:41:47 +0900

    예제 11

--- src/ch06_for/Ex11_ForEx.java
+++ src/ch06_for/Ex11_ForEx.java
@@ -11,6 +11,7 @@ public class Ex11_ForEx {
         * ****
         * *****
         */
+
         for (int i=1; i<=5; i++){
            for (int j=1; j<=i; j++){
                System.out.print("*");
