f4c125956c9a79d04a968fef05be1cc017b1ebf3
Taein142 <xodlsdldy@naver.com>
2023-12-05 15:57:40 +0900

    모래시계 만들었는데 이해가 아직 안감...

--- src/ch06_for/Ex11_ForEx.java
+++ src/ch06_for/Ex11_ForEx.java
@@ -11,11 +11,28 @@ public class Ex11_ForEx {
         * ****
         * *****
         */
+//        for (int i=1; i<=5; i++){   // 줄수, 별을 출력할 횟수
+//           for (int j=1; j<=i; j++){   // 별을 줄 수 만큼 출력
+//               System.out.print("*");
+//           }
+//            System.out.print("\n");   // 줄을 바꾸기 위한 용도
+//        }
 
+//        심화 모래시계 별찍기
+        for (int i=5; i>=1; i--){
+            for (int j=0; j<5-i; j++){
+                System.out.print(" ");
+            }for (int j=1; j<=(i*2)-1; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
         for (int i=1; i<=5; i++){
-           for (int j=1; j<=i; j++){
-               System.out.print("*");
-           }
+            for (int j=1; j<=5-i; j++) {
+                System.out.print(" ");
+            }for(int j=1; j<=(i*2)-1; j++){
+                System.out.print("*");
+            }
             System.out.print("\n");
         }
     }
