4db5b127c3bd2c30e3d30335230f0fbef51bb5a8
Taein142 <xodlsdldy@naver.com>
2023-12-07 09:30:49 +0900

    은행

---
+++ src/ch07_while/Ex08_WhileEx.java
@@ -0,0 +1,42 @@
+package ch07_while;
+
+import java.util.Scanner;
+
+public class Ex08_WhileEx {
+    public static void main(String[] args) {
+        int m = 213613;
+        Boolean run = true;
+        int sum = m;
+//        System.out.println("현재 잔액은 " + m + " 원입니다.");
+        Scanner scanner = new Scanner(System.in);
+
+        while (run) {
+            System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+            System.out.print("선택>");
+            int n = scanner.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.print("예금액> ");
+                    int a = scanner.nextInt();
+                    sum = sum + a;
+                    System.out.println("현재 잔액은 " + sum + "원입니다.");
+                    break;
+                case 2:
+                    System.out.print("출금액> ");
+                    int b = scanner.nextInt();
+                    sum = sum - b;
+                    System.out.println("현재 잔액은 " + sum + "원입니다.");
+                    break;
+                case 3:
+                    System.out.println("젼재 잔액은 " + sum + "원입니다.");
+                    break;
+                case 4:
+                    System.out.println("종료합니다.");
+                    run = false;
+                    break;
+
+            }
+        }
+
+    }
+}
