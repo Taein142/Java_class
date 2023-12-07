6c0d08e585813ebab3b5caf916a5df8293b112b3
Taein142 <xodlsdldy@naver.com>
2023-12-07 14:42:26 +0900

    배열 예제2

---
+++ src/ch09_array/Ex03_ArrayEx.java
@@ -0,0 +1,24 @@
+package ch09_array;
+
+import java.util.Scanner;
+
+public class Ex03_ArrayEx {
+    public static void main(String[] args) {
+        /*
+        크기가 3인 정수형 배열을 선언하고
+        실행 했을 때 스캐너로 정수값을 읿력받아 배열에 저장
+        배열에 저장된 값의 총합, 평균 계산
+        */
+        int sum =0;
+        int ave;
+        int[] numbers1 = new int[3];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < numbers1.length; i++) {
+            numbers1[i] = scanner.nextInt();
+            sum += numbers1[i];
+        }
+        System.out.println("저장소 numbers에 저장된 값의 총합은 " + sum + " 이고,");
+        ave = sum/ numbers1.length;
+        System.out.println("평균은 " + ave + " 이다.");
+    }
+}
