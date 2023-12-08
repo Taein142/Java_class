e37b679742031e2324936d2654b2872c5f9284e9
Taein142 <xodlsdldy@naver.com>
2023-12-08 10:05:31 +0900

    배열 예제5번 거스름돈 세기

---
+++ src/ch09_array/Ex05_Array.java
@@ -0,0 +1,42 @@
+package ch09_array;
+
+import java.util.Scanner;
+
+public class Ex05_Array {
+    public static void main(String[] args) {
+        /*거스름돈 계산하기
+         * 필요한 거스름돈을 입력했을 때 각 동전별로 몇 개가 필요한지 계산
+         * 예)
+         *       170 이라고 입력하면
+         *       100원 1개
+         *       50원 1개
+         *       10원 2개
+         *
+         *       780 이라고 입력하면
+         *       500원 1개
+         *       50원 1개
+         *       10원 3개
+         */
+        int[] coin = {500, 100, 50, 10};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("거스름돈 입력 : ");
+        int charge = scanner.nextInt();
+        for (int i = 0; i < coin.length; i++) {
+            System.out.println(coin[i] + "원 " + (charge / coin[i]) + "개");
+            charge %= coin[i]; // 입력했던 값이 나머지 값이 되도록 재정의
+        }
+
+        /*
+        // 선생님의 답안
+        int change = scanner.nextInt();
+        int count = 0;
+        // 거스름돈 / 동전 = 몫(갯수)
+        for (int i = 0; i< coin.length; i++){
+            count = change / coin[i];
+            System.out.println(coin[i] + "원 동전: " + count + "개");
+            change = change % coin[i];
+        }
+        // ... 어라? 똑같네?
+         */
+    }
+}
