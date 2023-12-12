c3fad5f247c99af39613917b33ee4fcf29eceebc
Taein142 <xodlsdldy@naver.com>
2023-12-12 15:31:53 +0900

    객체지향 setter  getter

--- src/ch10_class/ex8/BookMain.java
+++ src/ch10_class/ex8/BookMain.java
@@ -4,5 +4,12 @@ public class BookMain {
     public static void main(String[] args) {
         Book book = new Book();
 //        book.bookTitle = "책제목1";
+        book.setBookTitle("판도라하츠");
+        String title = book.getBookTitle();
+        System.out.println("title = " + title);
+        System.out.println(book.getBookTitle());
+
+        book.setBookAuthor("Jun Mochizuki");
+        System.out.println(book.getBookAuthor());
     }
 }
