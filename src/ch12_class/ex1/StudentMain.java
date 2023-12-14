199b53c4e2a9a03c8efcc69fda8e03d15b1b6246
Taein142 <xodlsdldy@naver.com>
2023-12-14 16:11:54 +0900

    클래스 분할하기 이제 시작합니다....

---
+++ src/ch12_class/ex1/StudentMain.java
@@ -0,0 +1,33 @@
+package ch12_class.ex1;
+
+import java.util.Scanner;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        int selectNo = 0;
+        // StudentService 클래스 객체 선언
+        StudentService studentService = new StudentService();
+
+        while (run) {
+            System.out.println("------------------------------------------------------");
+            System.out.println("1. method1호출 | 2.method2호출 | 3.method3호출 | 4. 종료");
+            System.out.println("------------------------------------------------------");
+            System.out.print("선택> ");
+            selectNo = scanner.nextInt();
+
+            if (selectNo == 1) {
+                System.out.println("호출전");
+                studentService.method1();
+                System.out.println("호출후");
+            } else if (selectNo == 2) {
+                studentService.method2();
+            } else if (selectNo == 3) {
+                studentService.method3();
+            } else if (selectNo == 4) {
+                run = false;
+            }
+        }
+    }
+}
---
+++ src/ch12_class/ex1/StudentRepository.java
@@ -0,0 +1,22 @@
+package ch12_class.ex1;
+
+public class StudentRepository {
+    public void method1(){
+        System.out.println("StudentRepository.method1");
+    }
+
+    /*
+    * method name: method2
+    * parameter: String
+    * return:x
+    * 실험내용: 전달받은 매개변수 값 출력
+    */
+
+    public void method2(String str1) {
+        System.out.println("str1 = " + str1);
+    }
+
+//    public void method3() {
+//        System.out.println("StudentRepository.method3");
+//    }
+}
