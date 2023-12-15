5dad203519828ab3d6bc5038e884c557152696c4
Taein142 <xodlsdldy@naver.com>
2023-12-15 11:49:34 +0900

    클래스 분할하기 오전타임 student 종료

--- src/ch12_class/ex1/StudentService.java
+++ src/ch12_class/ex1/StudentService.java
@@ -28,7 +28,7 @@ public class StudentService {
      * StudentDTO 객체를 생성하고
      * Repository의 method3로 DTO 객체를 전달(호출)함.
      */
-    public void method3() {
+    public void method3() { // 학생 정보 입력
         System.out.println("StudentService.method3");
         StudentDTO studentDTO = new StudentDTO("학생1", "20231111", "컴퓨터공학", "010-1111-1111");
 //        StudentDTO studentDTO = new StudentDTO();
@@ -44,7 +44,7 @@ public class StudentService {
     /**
      * Repository로 부터 List를 리턴 받아 for문으로 출력
      */
-    public void method4() {
+    public void method4() { // 전체 학생의 정보 출력
         List<StudentDTO> studentDTOList = studentRepository.method4();
         for (StudentDTO studentDTO: studentDTOList) {
             System.out.println("studentDTO = " + studentDTO);
@@ -57,10 +57,13 @@ public class StudentService {
     repository로 부터 id에 해당하는 학생정보를 리턴받고
     학생 정보를 출력
      */
-    public void method5(){
+    public void method5(){ // 특정 학생의 정보 출력
         System.out.println("조회할 id: ");
-        Long id = scanner.nextLong();
+        Long id = scanner.nextLong();  // 매칭할 id 입력
         StudentDTO studentDTO = studentRepository.method5(id);
+        // repository의 메서드5를 불려오면서 id 값을 전달하며 호출
+        // 그리고 그 값을 studentDTO의 클래스 타입을 가지고 있는
+        // 객체 student DTO를 생성하며 넣어줌.
         if (studentDTO !=null){
             // 조회결과 있음
             System.out.println("studentDTO = " + studentDTO);
@@ -68,6 +71,6 @@ public class StudentService {
             // 조회결과 없음
             System.out.println("요청하신 정보를 찾을 수 없습니다.");
         }
-        System.out.println("studentDTO = " + studentDTO);
+        System.out.println("studentDTO = " + studentDTO); // 정보결과 확인용
     }
 }
