960e685405c35074a65d833809c8c19a2fd1cd07
Taein142 <xodlsdldy@naver.com>
2023-12-26 15:55:56 +0900

    상속

--- src/ch14_inheritance/ex01/InheritanceMain.java
+++ src/ch14_inheritance/ex01/InheritanceMain.java
@@ -12,5 +12,17 @@ public class InheritanceMain {
         childClass.setName("자식이름1");
         childClass.setAge(100);
         System.out.println("childClass = " + childClass);
+        childClass.hello();
+
+        System.out.println("============");
+        // 좌변: 부모타입의 객체, 우변: 자식클래스 tod성자
+        ParentClass parentClass1 = new ChildClass();
+        parentClass1.hello();;
+        parentClass1 = new ParentClass();
+        parentClass1.hello();;
+
+        // 좌변: 자식타입의 객체, 우변: 부모클래스 생성자
+//        ChildClass childClass1 = (ChildClass) new ParentClass();
+        // 부모가 더 크기에 작은 자식에게 들어갈 수 없다. 하려면 형변환
     }
 }
